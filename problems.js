6a1f869acd4a4a0f05763adce816571c70187187
Shirley Martínez <97803763+SBMV8@users.noreply.github.com>
2022-12-02 13:26:06 -0500

    first commit

---
+++ problems.js
@@ -0,0 +1,20 @@
+count = 0
+while (count < 3)
+{
+  count = count + 1
+  console.log("Hola mundo")
+}
+
+var num1 = 5;
+var num2 = 10;
+let resultado = num1 + num2;
+console.log(" La suma de 5+10 es "+resultado);
+console.log("\n-------------------------") // Salto de linea
+
+const names = ['Aiden', 'Heather', 'Michael']
+console.log(names.length)
+for(let i=0;i<3;i++)
+{
+  console.log("El nombre numero "+ (i+1) + " es: " + names[i])
+}
+console.log(names)
